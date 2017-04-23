9a0b4e2877087432065d546e6a3767455aaa1244
MichalZakrzewski <michalzakrzewski@debian>
2017-04-23 22:34:50 +0200

    Further fixes, changes etx

--- cw06/zad1/client.c
+++ cw06/zad1/client.c
@@ -8,10 +8,10 @@ void finish(int signo){
 }
 
 int main(int argc, char *argv[]){
-	char *path = calloc(128, sizeof(char));
-	path = getenv("HOME");
-	key_t key = ftok(path, PROJECTID);
-	queue = msgget(key, 0);
+	//char *path = calloc(128, sizeof(char));
+	//path = getenv("HOME");
+	//key_t key = ftok(getenv("HOME"), PROJECTID);
+	queue = msgget(ftok(getenv("HOME"), PROJECTID), 0777);
 	signal(SIGINT, finish);
 	int privq;
 	if((privq = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL | 0777))==-1){
@@ -37,45 +37,45 @@ int main(int argc, char *argv[]){
 		printf("\n>>> ");
 		fgets(line, 128, stdin);
 		strcpy(tmp, line);
-		buf = strtok(tmp, " \n");
+		buf = strtok(tmp, " \n\0");
 		if(strcmp(buf, "echo")==0){
 			query.mtype = ECHO;
-			buf = strtok(NULL, "\n");
-			//query.mtext = realloc(query.mtext,(strlen(buf)+1)*sizeof(char));
-			query.mtext = buf;
-			//strcpy(query.mtext, buf);
-			msgsnd(queue, &query, MSGSIZE, MSG_NOERROR);
-			usleep(100);
-			msgrcv(privq, &response, MSGSIZE, 0, MSG_NOERROR);
+			buf = strtok(NULL, "\n\0");
+			query.mtext = realloc(query.mtext,(strlen(buf)+1)*sizeof(char));
+			//query.mtext = buf;
+			strcpy(query.mtext, buf);
+			msgsnd(queue, &query, MSGSIZE, 0);
+			//usleep(100);
+			msgrcv(privq, (void*)&response, MSGSIZE, 0, 0);
 			printf("%s\n", response.mtext);
 		}else if(strcmp(buf, "wers")==0){
 			query.mtype = WERS;
-			buf = strtok(NULL, "\n");
+			buf = strtok(NULL, "\n\0");
 			//query.mtext = realloc(query.mtext, (strlen(buf)+1)*sizeof(char));
 			//strcpy(query.mtext, buf);
 			query.mtext = buf;
-			msgsnd(queue, &query, MSGSIZE, MSG_NOERROR);
-			usleep(100);
-			msgrcv(privq, &response, MSGSIZE, 0, MSG_NOERROR);
+			msgsnd(queue, &query, MSGSIZE, 0);
+			//usleep(100);
+			msgrcv(privq, (void*)&response, MSGSIZE, 0, 0);
 			printf("\n%s\n", response.mtext);
 		}else if(strcmp(buf, "time")==0){
 			query.mtype = TIME;
-			msgsnd(queue, &query, MSGSIZE, MSG_NOERROR);
-			usleep(100);
-			msgrcv(privq, &response, MSGSIZE, 0, MSG_NOERROR);
+			msgsnd(queue, &query, MSGSIZE, 0);
+			//usleep(100);
+			msgrcv(privq, (void*)&response, MSGSIZE, 0, 0);
 			printf("\n%s\n", response.mtext);
 		}else if(strcmp(buf, "stop")==0){
 			query.mtype = STOP;
-			msgsnd(queue, &query, MSGSIZE, MSG_NOERROR);
+			msgsnd(queue, &query, MSGSIZE, 0);
 		}else if(strcmp(buf, "exit")==0){
 			msgctl(privq, IPC_RMID, NULL);
 			exit(0);
 		}
 		
-		if(buf!=NULL) free(buf);
-		if(line!=NULL) free(line);
-		if(query.mtext!=NULL) free(query.mtext);
-		if(response.mtext!=NULL) free(response.mtext);
+		//if(buf!=NULL) free(buf);
+		//if(line!=NULL) free(line);
+		//if(query.mtext!=NULL) free(query.mtext);
+		//if(response.mtext!=NULL) free(response.mtext);
 	}
 	return 0;
 }
